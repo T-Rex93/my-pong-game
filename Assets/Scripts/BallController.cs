3a17de8e6caa640af8ea3209366a89a874f8e406
Alvin Soejoedi <68222866+T-Rex93@users.noreply.github.com>
2024-05-05 22:38:55 +0700

    Added UI and Game Manager
    Created UI, Debugger, and Score Manager

--- Assets/Scripts/BallController.cs
+++ Assets/Scripts/BallController.cs
@@ -8,16 +8,20 @@ public class BallController : MonoBehaviour
     private Rigidbody2D rig;
 
     public Vector2 speed;
+    public Vector2 resetPosition;
 
-    // Start is called before the first frame update
     private void Start()
     {
         rig = GetComponent<Rigidbody2D>();
         rig.velocity = speed;
     }
 
-    // Update is called once per frame
     private void Update()
     {
+    }
+
+    public void ResetBall()
+    {
+        transform.position = new Vector3(resetPosition.x, resetPosition.y, 2);
     }
 }
